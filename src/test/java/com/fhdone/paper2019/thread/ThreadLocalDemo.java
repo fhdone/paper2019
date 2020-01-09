ceefe43b17667dfb40bd67f6a50949e752eaefaf
meteorafhd@gmail.com <meteorafhd@gmail.com>
2020-01-09 10:19:00 +0800

    demo update

--- src/test/java/com/fhdone/paper2019/thread/ThreadLocalDemo.java
+++ src/test/java/com/fhdone/paper2019/thread/ThreadLocalDemo.java
@@ -99,8 +99,8 @@ public class ThreadLocalDemo {
     @Test
     public  void test_3() throws Exception {
         // 0.创建线程池
-        final ThreadPoolExecutor bizPoolExecutor = new ThreadPoolExecutor(2, 2, 1, TimeUnit.MINUTES,
-                new LinkedBlockingQueue<>(1));
+        final ThreadPoolExecutor bizPoolExecutor = new ThreadPoolExecutor(2, 3, 1, TimeUnit.MINUTES,
+                new LinkedBlockingQueue<>());
 
         // 1 创建线程变量
         InheritableThreadLocal<Map<String,String>> inheritableThreadLocal = new InheritableThreadLocal<Map<String,String>>();
@@ -108,26 +108,38 @@ public class ThreadLocalDemo {
         map.put("key","value");
         inheritableThreadLocal.set(map);
 
-        // 2 投递三个任务
+        // 2.1 投递三个任务
         for (int i = 0; i < 3; ++i) {
             bizPoolExecutor.execute(() -> {
                 try {
-                    System.out.println("child:" + inheritableThreadLocal.get().get("key"));
-                    Thread.sleep(3000);
-
+                    System.out.println("child_1:" + inheritableThreadLocal.get().get("key"));
                     Map<String,String> map2 = new HashMap<>();
-                    map2.put("key","value2");
+                    map2.put("key","child_1+value1");
                     inheritableThreadLocal.set(map2);
-
+                    Thread.sleep(3000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             });
         }
 
+        // 2.2 投递三个任务
+        for (int i = 0; i < 3; ++i) {
+            bizPoolExecutor.execute(() -> {
+                try {
+                    System.out.println("child_2:" + inheritableThreadLocal.get().get("key"));
+                    Map<String,String> map2 = new HashMap<>();
+                    map2.put("key","child_2_value2");
+                    inheritableThreadLocal.set(map2);
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
 
         // 3休眠4s
-        Thread.sleep(4000);
+        Thread.sleep(10000);
 
         System.out.println("#######################");
         // 4.设置线程变量
@@ -160,24 +172,28 @@ public class ThreadLocalDemo {
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                System.out.println("========");
                 System.out.println(transmittableThreadLocal.get().name);
                 transmittableThreadLocal.set(new Span("zhangzhangzhang"));
                 System.out.println(transmittableThreadLocal.get().name);
             }
         };
 
+        System.out.println("##### child thread #####");
         ExecutorService executorService = Executors.newFixedThreadPool(1);
         executorService.submit(runnable);
         TimeUnit.SECONDS.sleep(1);
 
-        executorService.submit(runnable);
+        System.out.println("##### parent thread #####");
+        System.out.println(transmittableThreadLocal.get().name);
+        transmittableThreadLocal.set(new Span("xiexiexie2"));
+
+        System.out.println("##### child thread #####");
+        executorService.submit(runnable);    //自线程数据没变
         TimeUnit.SECONDS.sleep(1);
 
-        System.out.println("========");
-        System.out.println(transmittableThreadLocal.get().name);
 
-        Runnable ttlRunnable = TtlRunnable.get(runnable);
+        System.out.println("##### ttl thread #####");
+        Runnable ttlRunnable = TtlRunnable.get(runnable);  //TTL数据变了
         executorService.execute(ttlRunnable);
 
         executorService.shutdown();
