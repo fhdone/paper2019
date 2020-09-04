720f329600c1d07591763c112f4601e1e71856cf
meteorafhd@gmail.com <meteorafhd@gmail.com>
2020-09-04 15:14:08 +0800

    add elastic search

--- src/main/java/com/fhdone/paper2019/mangodb/MangoConfiguration.java
+++ src/main/java/com/fhdone/paper2019/mangodb/MangoConfiguration.java
@@ -1,20 +1,21 @@
 package com.fhdone.paper2019.mangodb;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ReadPreference;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.connection.ConnectionPoolSettings;
+import com.mongodb.connection.SocketSettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
+import java.util.concurrent.TimeUnit;
+
 @Configuration
 public class MangoConfiguration extends AbstractMongoClientConfiguration {
-
-    @Override
-    @Bean(name = "mongoClient")
-    public MongoClient mongoClient() {
-        return MongoClients.create("mongodb://localhost:27017");
-    }
 
     @Override
     protected String getDatabaseName() {
@@ -23,7 +24,31 @@ public class MangoConfiguration extends AbstractMongoClientConfiguration {
 
     @Bean(name = "mongoTemplate")
     public MongoTemplate mongoTemplate() throws Exception {
-        return new MongoTemplate(mongoClient(), "mycol");
+        MongoClientSettings mongoSettingsProperties = getMongoClientSettings();
+        MongoClient mongoClient = MongoClients.create(mongoSettingsProperties);
+        return new MongoTemplate(mongoClient, "mycol");
+    }
+
+    private MongoClientSettings getMongoClientSettings() {
+        return MongoClientSettings.builder()
+                .applicationName("es-demo")
+//                .applyToSslSettings(sslBuilder ->
+//                        SslSettings.builder().
+//                                enabled(sslEnabled).
+//                                invalidHostNameAllowed(false).build())
+                .applyConnectionString(
+                        new ConnectionString("mongodb://localhost:27017")
+                )
+                .applyToConnectionPoolSettings(connPoolBuilder ->
+                        ConnectionPoolSettings.builder().
+                                maxWaitTime(30, TimeUnit.SECONDS).
+                                maxSize(10).
+                                maxSize(20).build())
+                .applyToSocketSettings(socketBuilder ->
+                        SocketSettings.builder().
+                                connectTimeout(30,TimeUnit.SECONDS).build())
+                .readPreference(ReadPreference.secondaryPreferred())
+                .build();
     }
 
 }
